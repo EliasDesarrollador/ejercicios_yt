3edbb918f0a3cccad4fcfa716d674d20a6499df2
Elias Ortiz <elortiz@uaa.edu.py>
2025-11-14 14:14:52 -0300

    ejericio 1 comienzo

---
+++ ejercicio1.py
@@ -0,0 +1,16 @@
+
+# Resolver el siguiente  ejercicio
+# Ejercicio 1
+
+# 1. Definir una función max() que tome como argumento dos numeros y
+#devuelva el mayor de ellos.
+#(Es cierto que python tiene una función max() incorporada,
+#pero hacerla nosotros mismos es un muy buen ejercicio.
+ 
+def f_max(n1: int ,n2: int):
+    """Dado dos numeros de entrada retorna el maximo de ambos
+
+    Args:
+        n1 (int): primer numero a comparar
+        n2 (int): segundo numero a comparar
+    """
