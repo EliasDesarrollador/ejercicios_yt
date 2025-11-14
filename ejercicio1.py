21aa7ad0fdeebce568184f35dc4beda64b2cbb99
Elias Ortiz <elortiz@uaa.edu.py>
2025-11-14 14:22:40 -0300

    funcion para definir mayor  de dos numeros enteros

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,11 +6,19 @@
 #devuelva el mayor de ellos.
 #(Es cierto que python tiene una función max() incorporada,
 #pero hacerla nosotros mismos es un muy buen ejercicio.
- 
-def f_max(n1: int ,n2: int):
+def custom_max(n1: int ,n2: int):
     """Dado dos numeros de entrada retorna el maximo de ambos
 
     Args:
         n1 (int): primer numero a comparar
         n2 (int): segundo numero a comparar
+    Returns:
+        int: Mayor  de ambos
     """
+    if n2 > n1:
+        return n2
+    else:
+        n1
+
+print(custom_max(7,4))
+ 
